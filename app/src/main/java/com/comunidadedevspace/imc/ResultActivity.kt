98005490402aec61f613d41c24b58661491e9462
Elize Piovesan <elizepiovesan.sci@gmail.com>
2025-03-09 20:18:26 -0300

    -inclusão de informação extra sobre a classificação. -criação de uma caixa de diálogo para exibir as informações. -customização da caixa de diálogo.

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -1,15 +1,19 @@
 package com.comunidadedevspace.imc
 
 import android.R.color
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
 const val KEY_CLASSIFICACAO_IMC = "ResultActivity.KEY_IMC_CLASSIF"
@@ -52,8 +56,38 @@ class ResultActivity : AppCompatActivity() {
             // fechar a tela de Result Activity para evitar empilhamento
             finish()
 
+        }
+        val ic_info = findViewById<ImageView>(R.id.ic_info)
+
+        ic_info.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this, R.style.CustomAlertDialog)
+            builder
+                //icone de informação
+                .setIcon(R.drawable.ic_info_2)
+                // titulo
+                .setTitle("Classificação")
+                // botao ok
+                .setNeutralButton("OK"){dialog, which ->}
+                // texto exibido
+                .setItems(arrayOf("MAGREZA: menor que 18.5",
+                                  "NORMAL: de 18.5 até 24.9",
+                                  "SOBREPESO: de 25.0 até 29.9",
+                                  "OBESIDADE: de 30.0 até 39.9",
+                                  "OBESIDADE GRAVE: maior que 40.0")
+                                 ){dialog, which ->}
+            val dialog: AlertDialog = builder.create()
+            dialog.window?.apply {
+                setGravity(Gravity.BOTTOM)
+                attributes.y = 350
+            }
+            dialog.show()
 
         }
+
+
+
+
+
 
     }
 
